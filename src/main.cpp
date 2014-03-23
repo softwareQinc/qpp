a67765671e01a5478fbeb2cdf1de7be6006a8ab7
Vlad Gheorghiu <vgheorgh@gmail.com>
2014-03-23 16:46:52 -0400

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -25,17 +25,17 @@ int main(int argc, char **argv)
 //	size_t nout = atoi(argv[2]); // discard the first ndiscarded qubits
 
 	size_t n = 9;
-	size_t nout = n-2; // trace down first n-1 qubits
+	size_t nout = n - 2; // trace down first n-1 qubits
 
 	size_t dim = pow(2, n); // total dimension
 
 	std::vector<size_t> dims;
 	std::vector<size_t> subsys;
 
-	for(size_t i=0; i<n; i++)
+	for (size_t i = 0; i < n; i++)
 		dims.push_back(2);
-	for(size_t i=0; i<nout; i++)
-			subsys.push_back(i);
+	for (size_t i = 0; i < nout; i++)
+		subsys.push_back(i);
 
 	// generate a random 2^n x 2^n matrix
 	cmat A = randn(dim);
@@ -43,29 +43,28 @@ int main(int argc, char **argv)
 	// take the partial trace
 	cmat B = ptrace(A, subsys, dims);
 
-
 	cout << "Partial trace of " << nout << " out of " << n
 			<< " qubits. Total dimension=2^" << n << "=" << dim << "." << endl;
-	cout << endl << "Remaining matrix:"<<endl;
-	if(B.cols()<32)
+	cout << endl << "Remaining matrix:" << endl;
+	if (B.cols() < 32)
 		disp(B);
 	else
-		cout<<" Size too large to be useful :)";
+		cout << " Size too large to be useful :)";
 
-	cout<<endl<<"The eigenvalues of B are:"<<endl;
+	cout << endl << "The eigenvalues of B are:" << endl;
 	disp(evals(B));
-	cout<<endl<<"The eigenvectors of B are:"<<endl;
+	cout << endl << "The eigenvectors of B are:" << endl;
 	disp(evects(B));
 
-	cout<<endl;
+	cout << endl;
 
-	disp(1.0+ct::ii);
-	cout<<endl;
-	disp(1.0+ct::ii);
-	cout<<endl;
+	disp(1.0 + ct::ii);
+	cout << endl;
+	disp(1.0 + ct::ii);
+	cout << endl;
 	disp_container(dims);
-	cout<<endl;
+	cout << endl;
 	disp_container(dims);
-	cout<<endl;
+	cout << endl;
 
 }
