7edf551ac2cd3e1ce9ce124147de5a36e61a733e
Vlad Gheorghiu <vgheorgh@gmail.com>
2014-03-28 18:46:56 -0400

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main()
 	size_t statistics[] =
 	{ 0, 0, 0 };
 	size_t N = 100000;
-	for (int i = 0; i < N; i++)
+	for (size_t i = 0; i < N; i++)
 		statistics[d.sample()]++;
 
 	cout << (double) statistics[0] / (double) N << endl;
