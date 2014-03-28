8d6ea9fab75ed74f8cb320379709f9b46b6ebeee
Vlad Gheorghiu <vgheorgh@gmail.com>
2014-03-27 22:13:52 -0400

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -23,38 +23,4 @@ int main()
 {
 	_init(); // this will be done automatically in the framework
 
-	cmat psi(4, 1);
-	psi << 0.6, 0, 0, 0.8; // A Bell-state
-	psi = psi / norm(psi);
-
-	cmat rho = ptrace((cmat) (psi * adjoint(psi)),
-	{ 1 },
-	{ 2, 2 });
-
-	cout << shannon(rho) << endl << endl;
-
-	cmat vec(4, 1);
-	vec << 0.36, 0, 0, 0.64;
-	//cout<<expm(vec)<<endl;
-
-	cout << renyi(0.99, vec) << endl << endl;
-	cout << renyi(1.01, vec) << endl << endl;
-
-	cout << renyi(1500, vec) << endl << endl;
-	cout << renyi_inf(vec) << endl << endl;
-	cout << renyi_inf(rho) << endl << endl;
-
-	cmat u = rand_unitary(2);
-	disp(u);
-	cout << endl;
-	disp((cmat) (u * adjoint(u) - cmat::Identity(2, 2)));
-	cout << endl << endl;
-
-	cout << u * adjoint(u) - cmat::Identity(2, 2);
-
-	u << ct::ee, 0, 0, ct::ee;
-	cout << endl << endl;
-	cout << "Matrix log: " << endl;
-	cout << funm(u, std::log) << endl << endl;
-
 }
