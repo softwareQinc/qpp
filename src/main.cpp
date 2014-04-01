f573a59c3a24cd160304028188c7334418ef6eba
Vlad Gheorghiu <vgheorgh@gmail.com>
2014-04-01 17:50:00 -0400

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -29,18 +29,18 @@ int main()
 	_init();
 
 	// Display formatting
-	std::cout << std::fixed; // use fixed format for nice formatting
-//	std::cout << std::scientific;
-	std::cout << std::setprecision(4); // only for fixed or scientific modes
+	cout << std::fixed; // use fixed format for nice formatting
+//	cout << std::scientific;
+	cout << std::setprecision(4); // only for fixed or scientific modes
 
-	cout << "Starting qpp..." << endl << endl;
+	cout << "Starting qpp..." << endl;
 
 	size_t n = 12; // 12 qubits
 	size_t N = std::pow(2, n);
 	vector<size_t> dims; // local dimensions
 	for (size_t i = 0; i < n; i++)
 		dims.push_back(2);
-	std::cout << "n = " << n << " qubits, matrix size " << N << " x " << N
+	cout <<  "n = " << n << " qubits, matrix size " << N << " x " << N
 			<< "." << endl;
 
 	// TIMING
@@ -65,7 +65,7 @@ int main()
 	vector<size_t> subsys_ptrace = { 0 };
 	cout << "Subsytem(s): ";
 	internal::_disp_container(subsys_ptrace);
-	std::cout << endl;
+	cout << endl;
 	t.tic();
 	ptrace(randcmat, subsys_ptrace, dims);
 	t.toc();
@@ -78,7 +78,7 @@ int main()
 		subsys_ptranspose.push_back(i);
 	cout << "Subsytem(s): ";
 	internal::_disp_container(subsys_ptranspose);
-	std::cout << endl;
+	cout << endl;
 	t.tic();
 	ptranspose(randcmat, subsys_ptranspose, dims);
 	t.toc();
@@ -91,15 +91,15 @@ int main()
 		perm.push_back((i + 1) % n);
 	cout << "Subsytem(s): ";
 	internal::_disp_container(perm);
-	std::cout << endl;
+	cout << endl;
 	t.tic();
 	syspermute(randcmat, perm, dims);
 	t.toc();
 	cout << "Took " << t.seconds() << " seconds." << endl;
 
 	total.toc(); // read the total running time
-	cout << "Total time: " << total.seconds() << " seconds." << endl;
+	cout << endl << "Total time: " << total.seconds() << " seconds." << endl;
 	// END TIMING
 
-	cout << endl << "Exiting qpp..." << endl;
+	cout << "Exiting qpp..." << endl;
 }
