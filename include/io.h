9176e7e947f4066379015ae503b899520c1eb314
Vlad Gheorghiu <vgheorgh@gmail.com>
2014-04-07 01:44:06 -0400

    commit

--- include/io.h
+++ include/io.h
@@ -162,6 +162,36 @@ void displn(const types::DynMat<Scalar> &A, double chop = ct::chop,
 	os << std::endl;
 }
 
+// Displays a ket in friendly form
+void disp(const types::ket &A, double chop = ct::chop, std::ostream& os =
+		std::cout)
+{
+	disp(static_cast<types::cmat>(A));
+}
+
+// Displays a ket in friendly form
+// and adds new line
+void displn(const types::ket &A, double chop = ct::chop, std::ostream& os =
+		std::cout)
+{
+	displn(static_cast<types::cmat>(A));
+}
+
+// Displays a bra in friendly form
+void disp(const types::bra &A, double chop = ct::chop, std::ostream& os =
+		std::cout)
+{
+	disp(static_cast<types::cmat>(A));
+}
+
+// Displays a bra in friendly form
+// and adds new line
+void displn(const types::bra &A, double chop = ct::chop, std::ostream& os =
+		std::cout)
+{
+	displn(static_cast<types::cmat>(A));
+}
+
 // Displays a complex number in friendly form
 void disp(const types::cplx c, double chop = ct::chop, std::ostream& os =
 		std::cout)
