b646f43a0af66f87d70baf16fa30d82d49a55bd5
vsoftco <vsoftco@gmail.com>
2014-11-10 17:47:05 -0500

    commit

--- example.cpp
+++ example.cpp
@@ -182,15 +182,15 @@ int main()
         ket b1 = codes.codeword(Codes::Type::SEVEN_QUBIT_STEANE, 1);
         ket c0 = codes.codeword(Codes::Type::NINE_QUBIT_SHOR, 0);
         ket c1 = codes.codeword(Codes::Type::NINE_QUBIT_SHOR, 1);
-        std::cout << ">> Five qubit [[5, 1, 3]] code. ";
+        std::cout << ">> [[5, 1, 3]] Five qubit code. ";
         std::cout << "Checking codeword orthogonality." << std::endl;
         std::cout << ">> <0L | 1L> = ";
         std::cout << disp(adjoint(a0) * a1) << std::endl;
-        std::cout << ">> Seven qubit [[7, 1, 3]] Steane code. ";
+        std::cout << ">> [[7, 1, 3]] Seven qubit Steane code. ";
         std::cout << "Checking codeword orthogonality." << std::endl;
         std::cout << ">> <0L | 1L> = ";
         std::cout << disp(adjoint(b0) * b1) << std::endl;
-        std::cout << ">> Nine qubit [[9, 1, 3]] Shor code. ";
+        std::cout << ">> [[9, 1, 3]] Nine qubit Shor code. ";
         std::cout << "Checking codeword orthogonality." << std::endl;
         std::cout << ">> <0L | 1L> = ";
         std::cout << disp(adjoint(c0) * c1) << std::endl;
