01a83e9ae7969cdb1b15b2ba7f2931bff3f38076
vsoftco <vsoftco@gmail.com>
2014-11-10 17:22:50 -0500

    gcd and lcm added

--- html/number__theory_8h.js
+++ html/number__theory_8h.js
@@ -2,5 +2,9 @@ var number__theory_8h =
 [
     [ "contfrac2x", "number__theory_8h.html#a117454e14ef9b63f568bfc28d712eb07", null ],
     [ "contfrac2x", "number__theory_8h.html#ac3095e8de98c536e5e620b6a0582e1b5", null ],
+    [ "gcd", "number__theory_8h.html#ae245a12a1f7c9ab36da26498b2c4da96", null ],
+    [ "gcd", "number__theory_8h.html#ac6b06a1ed3f09f5296470d4c180d9edc", null ],
+    [ "lcm", "number__theory_8h.html#a37ded4330bac75ed34190b1c40d53381", null ],
+    [ "lcm", "number__theory_8h.html#a0cd094d5bde1344756f98167abb079f5", null ],
     [ "x2contfrac", "number__theory_8h.html#a7a833b3c059da10f018372fd34fc5108", null ]
 ];
