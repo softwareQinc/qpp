67322bd967d9f3724b8cfe57b21d9a2b2d5f1c60
vsoftco <vsoftco@gmail.com>
2014-11-10 21:59:29 -0500

    commit

--- html/number__theory_8h.js
+++ html/number__theory_8h.js
@@ -1,10 +1,10 @@
 var number__theory_8h =
 [
-    [ "contfrac2x", "number__theory_8h.html#a117454e14ef9b63f568bfc28d712eb07", null ],
-    [ "contfrac2x", "number__theory_8h.html#ac3095e8de98c536e5e620b6a0582e1b5", null ],
+    [ "contfrac2x", "number__theory_8h.html#a1511e4d5c07691627c88e66070873b1b", null ],
+    [ "contfrac2x", "number__theory_8h.html#a155296775ac0c0cd0cfe52c2ade8f14c", null ],
     [ "gcd", "number__theory_8h.html#ae245a12a1f7c9ab36da26498b2c4da96", null ],
     [ "gcd", "number__theory_8h.html#ac6b06a1ed3f09f5296470d4c180d9edc", null ],
     [ "lcm", "number__theory_8h.html#a37ded4330bac75ed34190b1c40d53381", null ],
     [ "lcm", "number__theory_8h.html#a0cd094d5bde1344756f98167abb079f5", null ],
-    [ "x2contfrac", "number__theory_8h.html#a7a833b3c059da10f018372fd34fc5108", null ]
+    [ "x2contfrac", "number__theory_8h.html#a569c5700863d7054bea8e36f26d9930b", null ]
 ];
