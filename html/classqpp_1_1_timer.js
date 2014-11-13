1aba3d596a8b234207ab5e8c4cf90b8c735fecfd
vsoftco <vsoftco@gmail.com>
2014-11-13 15:56:08 -0500

    code formatting

--- html/classqpp_1_1_timer.js
+++ html/classqpp_1_1_timer.js
@@ -4,7 +4,7 @@ var classqpp_1_1_timer =
     [ "seconds", "classqpp_1_1_timer.html#aaaa3d575c39a0cc38bf2291e723a1cea", null ],
     [ "tic", "classqpp_1_1_timer.html#ae1bb3eb8a0f7851ba52005fab35e1c03", null ],
     [ "toc", "classqpp_1_1_timer.html#ae85c057c8fdc095b0a06feefe0691a1a", null ],
-    [ "operator<<", "classqpp_1_1_timer.html#ac925ce7106d65680a2d32ae33e6f892b", null ],
+    [ "operator<<", "classqpp_1_1_timer.html#ab9d2ca93425b03884be70e2938019391", null ],
     [ "_end", "classqpp_1_1_timer.html#a9a7916516dfc50e4eb8c4e45a9ef9358", null ],
     [ "_start", "classqpp_1_1_timer.html#a63f565f4d3000f83acb5208ab6b123da", null ]
 ];
