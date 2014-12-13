1703036432faa388525fa46d838d1f91a4e5d5df
vsoftco <vsoftco@gmail.com>
2014-12-12 19:56:56 -0500

    final commit v0.1

--- doc/html/navtree.js
+++ doc/html/navtree.js
@@ -1,7 +1,7 @@
 var NAVTREE =
 [
   [ "Quantum++", "index.html", [
-    [ "Development branch, use it at your own risk!", "md___users_vlad__dropbox_programming_cpp_qpp_clion__r_e_a_d_m_e.html", null ],
+    [ "Quantum++", "md___users_vlad__dropbox_programming_cpp_qpp_clion__r_e_a_d_m_e.html", null ],
     [ "Namespaces", null, [
       [ "Namespace List", "namespaces.html", "namespaces" ],
       [ "Namespace Members", "namespacemembers.html", [
