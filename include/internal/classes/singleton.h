d2c5137f7fbf039e8ba8c731549d723d9b52d821
vsoftco <vsoftco@gmail.com>
2015-03-11 01:22:57 -0400

    commit

--- include/internal/classes/singleton.h
+++ include/internal/classes/singleton.h
@@ -71,17 +71,21 @@ namespace internal // internal class, do not modify
 template<typename T>
 class Singleton
 {
+
+protected:
     // prevents deleting pointer to instance
-    void operator delete(void*)
+    static void operator delete(void*)
     {
     }
 
-protected:
     Singleton() noexcept = default;
 
     Singleton(const Singleton&) = delete;
 
     Singleton& operator=(const Singleton&) = delete;
+
+    virtual ~Singleton() = default; // to silence base class Singleton<T> has a
+                                    // non-virtual destructor [-Weffc++]
 
 public:
     static T& get_instance() noexcept(std::is_nothrow_constructible<T>::value)
