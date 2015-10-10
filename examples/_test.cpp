6b9eecff2bd7b5f75af432bf17a033a0dd662259
vsoftco <vsoftco@gmail.com>
2015-10-10 17:16:17 -0400

    commit

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -13,7 +13,7 @@ using std::endl;
 // TODO: modify the pure-states examples accordingly
 // TODO: check normalization in measure functions
 // TODO: add a duration-outputting member function to Timer
-// TODO: throw exception when d = 0
+// TODO: throw exceptions when d = 0
 
 // Future work
 // TODO: Implement a circuit description language, load/save from/to file
@@ -30,54 +30,54 @@ int main()
 
     // Testing experimental::_measure()
     {
-        std::cout << ">> experimental::_measure() ***\n";
+        cout << ">> experimental::_measure() ***\n";
         auto meas = experimental::_measure(psi, V, subsys);
         auto m = std::get<0>(meas);
         auto probs = std::get<1>(meas);
         auto outs = std::get<2>(meas);
 
-        std::cout << "Initial state: \n";
-        std::cout << disp(psi) << std::endl;
-        std::cout << "Subsystems: " << disp(subsys, " ") << std::endl;
-        std::cout << "Measurement matrix: \n";
-        std::cout << disp(V) << std::endl;
-        std::cout << "Probabilities: " << disp(probs, " ") << std::endl;
-        std::cout << "Possible output states: \n";
+        cout << "Initial state: \n";
+        cout << disp(psi) << endl;
+        cout << "Subsystems: " << disp(subsys, " ") << endl;
+        cout << "Measurement matrix: \n";
+        cout << disp(V) << endl;
+        cout << "Probabilities: " << disp(probs, " ") << endl;
+        cout << "Possible output states: \n";
         for (auto&& elem: outs)
-            std::cout << disp(elem) << std::endl << std::endl;
-        std::cout << "Result: " << m << std::endl;
-        std::cout << "Output state: \n";
-        std::cout << disp(outs[0]) << std::endl;
+            cout << disp(elem) << endl << endl;
+        cout << "Result: " << m << endl;
+        cout << "Output state: \n";
+        cout << disp(outs[0]) << endl;
     }
 
     // Testing experimental::_measure_seq()
     {
-        std::cout << std::endl << ">> experimental::_measure_seq()\n";
+        cout << endl << ">> experimental::_measure_seq()\n";
         auto meas = experimental::_measure_seq(psi, subsys);
         auto results = std::get<0>(meas);
         auto prob = std::get<1>(meas);
         auto out = std::get<2>(meas);
 
-        std::cout << "Initial state: \n";
-        std::cout << disp(psi) << std::endl;
-        std::cout << "Subsystems: " << disp(subsys, " ") << std::endl;
-        std::cout << "Measurement matrix: \n";
-        std::cout << disp(V) << std::endl;
-        std::cout << "Results: " << disp(results, " ") << std::endl;
-        std::cout << "Probability: " << prob << std::endl;
-        std::cout << "Output state: \n" << disp(out) << std::endl;
+        cout << "Initial state: \n";
+        cout << disp(psi) << endl;
+        cout << "Subsystems: " << disp(subsys, " ") << endl;
+        cout << "Measurement matrix: \n";
+        cout << disp(V) << endl;
+        cout << "Results: " << disp(results, " ") << endl;
+        cout << "Probability: " << prob << endl;
+        cout << "Output state: \n" << disp(out) << endl;
     }
 
     // Testing experimental::ip()
     {
-        std::cout << std::endl << ">> experimental::ip() ***\n";
+        cout << endl << ">> experimental::ip() ***\n";
         psi = 0.8 * mket({0, 0}) + 0.6 * mket({1, 1});
-        auto phi = st.z0;
+        auto phi = st.y1;
         auto result = experimental::ip(phi, psi, {1});
-        std::cout << "Initial state: \n";
-        std::cout << disp(psi) << std::endl;
-        std::cout << "Subsystems: " << disp(subsys, " ") << std::endl;
-        std::cout << "Generalized inner product:\n";
-        std::cout << disp(result);
+        cout << "Initial state: \n";
+        cout << disp(psi) << endl;
+        cout << "Subsystems: " << disp(subsys, " ") << endl;
+        cout << "Generalized inner product:\n";
+        cout << disp(result);
     }
 }
