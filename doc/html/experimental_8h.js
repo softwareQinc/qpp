606977f8870c83ab54e8ee0ac45a42af63322d73
vsoftco <vsoftco@gmail.com>
2015-10-10 23:17:40 -0400

    commit

--- doc/html/experimental_8h.js
+++ doc/html/experimental_8h.js
@@ -4,6 +4,6 @@ var experimental_8h =
     [ "_measure", "experimental_8h.html#a08c7861e73ed5d5884037a4fabb078e4", null ],
     [ "_measure_seq", "experimental_8h.html#a474ff2c082f40738cb3fcc322197af32", null ],
     [ "_measure_seq", "experimental_8h.html#a738f9f1257a5609d44e98d99b04cd609", null ],
-    [ "ip", "experimental_8h.html#a5e6a8d9b6fa0a5ef065c04d408adf7f0", null ],
-    [ "ip", "experimental_8h.html#a0cbf1bdd3c2bab00f1ac1c140335f124", null ]
+    [ "ip", "experimental_8h.html#a79db6cc9dcb47ed62a1f2843ba1eaf67", null ],
+    [ "ip", "experimental_8h.html#a0c22e59a9cae158c2bffbbcd27e7022c", null ]
 ];
