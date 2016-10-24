c15b988fd9444b258a41c627e4c9ec2fcf7d4e60
Vlad Gheorghiu <vsoftco@gmail.com>
2016-10-24 15:21:43 -0400

    commit

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -10,7 +10,4 @@ int main()
     std::cout << "Testing...\n";
     PRINTLN("Testing debug messages");
     ERRORLN("Testing debug error messages");
-
-    std::cout << disp(st.plus(1)) << "\n\n";
-    std::cout << disp(st.zero(1)) << "\n\n";
 }
--- unit_tests/tests/classes/states.cpp
+++ unit_tests/tests/classes/states.cpp
@@ -41,7 +41,7 @@ TEST(qpp_States_jn, AllTests)
     EXPECT_NEAR(0, norm(kron(st.z0, st.z0) - qpp::st.jn(0, n)), 1e-7);
     EXPECT_NEAR(0, norm(kron(st.z1, st.z1) - qpp::st.jn(1, n)), 1e-7);
 
-    n = 10;
+    n = 4;
     EXPECT_NEAR(0, norm(kronpow(st.z0, n) - qpp::st.jn(0, n)), 1e-7);
     EXPECT_NEAR(0, norm(kronpow(st.z1, n) - qpp::st.jn(1, n)), 1e-7);
 
@@ -53,7 +53,7 @@ TEST(qpp_States_jn, AllTests)
     EXPECT_NEAR(0, norm(mket({1}, d) - qpp::st.jn(1, n, d)), 1e-7);
     EXPECT_NEAR(0, norm(mket({3}, d) - qpp::st.jn(3, n, d)), 1e-7);
 
-    n = 10;
+    n = 4;
     EXPECT_NEAR(0, norm(kronpow(mket({0}, d), n) - qpp::st.jn(0, n, d)), 1e-7);
     EXPECT_NEAR(0, norm(kronpow(mket({1}, d), n) - qpp::st.jn(1, n, d)), 1e-7);
     EXPECT_NEAR(0, norm(kronpow(mket({4}, d), n) - qpp::st.jn(4, n, d)), 1e-7);
@@ -100,7 +100,7 @@ TEST(qpp_States_minus, AllTests)
     EXPECT_NEAR(0, norm(kron(gt.H, gt.H) * mket({1, 1}) - qpp::st.minus(n)),
                 1e-7);
 
-    n = 10;
+    n = 4;
     EXPECT_NEAR(0, norm(kronpow(gt.H, n) * mket(std::vector<idx>(n, 1)) -
                         qpp::st.minus(n)), 1e-7);
 }
@@ -116,7 +116,7 @@ TEST(qpp_States_one, AllTests)
     n = 2;
     EXPECT_NEAR(0, norm(kron(st.z1, st.z1) - qpp::st.one(n)), 1e-7);
 
-    n = 10;
+    n = 4;
     EXPECT_NEAR(0, norm(kronpow(st.z1, n) - qpp::st.one(n)), 1e-7);
 
     // qudits
@@ -125,7 +125,7 @@ TEST(qpp_States_one, AllTests)
     n = 1;
     EXPECT_NEAR(0, norm(mket({1}, d) - qpp::st.one(n, d)), 1e-7);
 
-    n = 10;
+    n = 4;
     EXPECT_NEAR(0, norm(kronpow(mket({1}, d), n) - qpp::st.one(n, d)), 1e-7);
 }
 /******************************************************************************/
@@ -139,7 +139,7 @@ TEST(qpp_States_plus, AllTests)
     EXPECT_NEAR(0, norm(kron(gt.H, gt.H) * mket({0, 0}) - qpp::st.plus(n)),
                 1e-7);
 
-    n = 10;
+    n = 4;
     EXPECT_NEAR(0, norm(kronpow(gt.H, n) * mket(std::vector<idx>(n, 0)) -
                         qpp::st.plus(n)), 1e-7);
 }
@@ -155,7 +155,7 @@ TEST(qpp_States_zero, AllTests)
     n = 2;
     EXPECT_NEAR(0, norm(kron(st.z0, st.z0) - qpp::st.zero(n)), 1e-7);
 
-    n = 10;
+    n = 4;
     EXPECT_NEAR(0, norm(kronpow(st.z0, n) - qpp::st.zero(n)), 1e-7);
 
     // qudits
@@ -164,7 +164,7 @@ TEST(qpp_States_zero, AllTests)
     n = 1;
     EXPECT_NEAR(0, norm(mket({0}, d) - qpp::st.zero(n, d)), 1e-7);
 
-    n = 10;
+    n = 4;
     EXPECT_NEAR(0, norm(kronpow(mket({0}, d), n) - qpp::st.zero(n, d)), 1e-7);
 }
 /******************************************************************************/
