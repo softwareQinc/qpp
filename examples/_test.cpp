382850b3eddd81d2d94c65a49cad1418c37c79b9
Vlad Gheorghiu <vsoftco@gmail.com>
2016-12-11 18:33:03 -0500

    commit

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -9,30 +9,4 @@ using namespace qpp;
 int main()
 {
     std::cout << "Testing...\n";
-
-    std::cout << "Playing with exceptions...\n";
-    try
-    {
-        throw exception::UndefinedType{__FILE__ + std::string(" at line ") +
-                                       std::to_string(__LINE__)};
-    }
-    catch (exception::UndefinedType& e)
-    {
-        std::cout << e.what() << '\n';
-        std::cout << "Exception type: " << e.type_description() << '\n';
-    };
-
-
-    for (idx i = 0; i < 10; ++i)
-    {
-        std::cout << rand(bigint(0), 10) << " ";
-    }
-    std::cout << '\n';
-
-    std::ifstream fout("prng_state.txt");
-    rdevs.load(fout);
-    for (idx i = 0; i < 10; ++i)
-    {
-        std::cout << rand((bigint) 0, 100) << " ";
-    }
 }
