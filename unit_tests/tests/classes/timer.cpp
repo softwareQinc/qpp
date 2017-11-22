4ebfb8d121f8b4d2a6713a933365b9a65a1f3ee9
Vlad Gheorghiu <vsoftco@gmail.com>
2017-11-21 22:49:57 -0500

    commit

--- unit_tests/tests/classes/timer.cpp
+++ unit_tests/tests/classes/timer.cpp
@@ -31,7 +31,7 @@ using namespace qpp;
 // All test below test std::chrono::steady_clock timers,
 // i.e Timer<T, CLOCK_T = std::chrono::steady_clock>
 
-// the precision should be at least within 0.02s, i.e. 20 ms or 2000 micros
+// the precision should be at least within 0.05s <=> 50 ms <=> 50000 micros
 /******************************************************************************/
 /// BEGIN template<typename U = T> U qpp::Timer::get_duration() const noexcept
 TEST(qpp_Timer_get_duration, AllTests)
@@ -43,17 +43,17 @@ TEST(qpp_Timer_get_duration, AllTests)
     t1.toc(); // get current time snap
 
     auto duration_t1_s = t1.get_duration(); // in seconds
-    EXPECT_NEAR(duration_t1_s.count(), 1, 0.02); // within 0.02s
+    EXPECT_NEAR(duration_t1_s.count(), 1, 0.05); // within 0.05s
 
     auto duration_t1_ms = t1.get_duration<milliseconds>(); // in milli seconds
-    EXPECT_NEAR(duration_t1_ms.count(), 1000, 20); // within 0.02s
+    EXPECT_NEAR(duration_t1_ms.count(), 1000, 50); // within 0.05s
 
     Timer<std::chrono::microseconds> t2; // in micro seconds
     std::this_thread::sleep_for(microseconds(100000)); // sleep 0.1 seconds
     t2.toc(); // get current time snap
 
     auto duration_t2_micros = t2.get_duration();
-    EXPECT_NEAR(duration_t2_micros.count(), 100000, 20000); // within 0.02s
+    EXPECT_NEAR(duration_t2_micros.count(), 100000, 50000); // within 0.05s
 }
 /******************************************************************************/
 /// BEGIN void qpp::Timer::tic() noexcept
@@ -69,21 +69,21 @@ TEST(qpp_Timer_tic_tics_toc, AllTests)
     std::this_thread::sleep_for(milliseconds(100)); // sleep 0.1 seconds
     t.tic(); // reset the timer
     t.toc(); // get current time snap
-    EXPECT_NEAR(t.tics(), 0, 0.02); // within 0.02s
+    EXPECT_NEAR(t.tics(), 0, 0.05); // within 0.05s
 
     t.tic(); // reset
     // sleep an additional 0.1s (without reseting the timer)
     std::this_thread::sleep_for(milliseconds(100));
     t.toc(); // get current time snap
-    EXPECT_NEAR(t.tics(), 0.1, 0.02); // within 0.02s
+    EXPECT_NEAR(t.tics(), 0.1, 0.05); // within 0.05s
 
     std::this_thread::sleep_for(milliseconds(100));
     t.toc(); // get current time snap
-    EXPECT_NEAR(t.tics(), 0.2, 0.02); // within 0.02s
+    EXPECT_NEAR(t.tics(), 0.2, 0.05); // within 0.05s
 
     t.tic(); // reset
     std::this_thread::sleep_for(milliseconds(100));
     t.toc(); // get current time snap
-    EXPECT_NEAR(t.tics(), 0.1, 0.02); // within 0.02s
+    EXPECT_NEAR(t.tics(), 0.1, 0.05); // within 0.05s
 }
 /******************************************************************************/
