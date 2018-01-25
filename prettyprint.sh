4f3d4228e37fc432ccd13b9cb8c8bfa8b17b91c0
Vlad Gheorghiu <vsoftco@gmail.com>
2018-01-25 18:00:43 -0500

    commit

--- prettyprint.sh
+++ prettyprint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # Code beautifier with clang-format
+# Recursively parses the folder(s) passed as command line argument(s)
 
 CLANG_FORMAT=clang-format-3.9
 
-for folders in "$@"
+echo "Code formatting..."
+for folder in "$@"
 do
-    find $folders \( -iname '*.cpp' -o -iname '*.c' -o -iname '*.h'\
+    echo $folder 
+    find $folder \( -iname '*.cpp' -o -iname '*.c' -o -iname '*.h'\
          -o -iname '*.hpp' \) | xargs $CLANG_FORMAT -style=file -i
 done
