c0bef74dc61b1ed7f66fa0029f3b31811fdc8f5c
Vlad Gheorghiu <vsoftco@gmail.com>
2018-09-11 11:01:13 -0400

    commit

--- stress_tests/src/qft.cpp
+++ stress_tests/src/qft.cpp
@@ -17,26 +17,26 @@ int main(int argc, char **argv) {
     }
 
     idx num_cores = std::stoi(argv[1]); // number of cores
-    idx N = std::stoi(argv[2]);         // number of qubits
+    idx n = std::stoi(argv[2]);         // number of qubits
     omp_set_num_threads(num_cores);     // number of cores
 
-    std::vector<idx> qubits(N); // initial state
+    std::vector<idx> qubits(n); // initial state
     ket psi = mket(qubits);
     ket result = psi;
 
     Timer<> t; // start timing
-    for (idx i = 0; i < N; ++i) {
+    for (idx i = 0; i < n; ++i) {
         result = apply(result, gt.H, {i}); // apply Hadamard on qubit i
         // apply controlled rotations
-        for (idx j = 2; j <= N - i; ++j) {
+        for (idx j = 2; j <= n - i; ++j) {
             cmat Rj(2, 2);
             Rj << 1, 0, 0, omega(std::pow(2, j));
             result = applyCTRL(result, Rj, {i + j - 1}, {i});
         }
     }
     // we have the qubits in reversed order, we must swap them
-    for (idx i = 0; i < N / 2; ++i) {
-        result = apply(result, gt.SWAP, {i, N - i - 1});
+    for (idx i = 0; i < n / 2; ++i) {
+        result = apply(result, gt.SWAP, {i, n - i - 1});
     }
-    std::cout << num_cores << ", " << N << ", " << t.toc() << '\n';
+    std::cout << num_cores << ", " << n << ", " << t.toc() << '\n';
 }
