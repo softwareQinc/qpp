3422075faa2844a4501d128d1cec579d64dea006
Vlad Gheorghiu <vsoftco@gmail.com>
2018-11-26 17:48:25 -0500

    commit

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -6,4 +6,21 @@
 
 int main() {
     using namespace qpp;
+    idx d = 2;
+    idx n = 3;
+    idx D = std::pow(d, n);
+
+    ket input = randket(D);
+    ket result = QFT(input, d);
+    std::cout << norm(gt.Fd(D) * input - result) << '\n';
+
+    ket a = randket(D);
+    ket b = randket(D);
+    std::cout << norm(kron(a, b) - gt.SWAPd(D) * kron(b, a)) << "\n\n";
+
+    ket psi = 0000_ket;
+    auto x = applyQFT(prj(psi), {3, 1, 2});
+    auto y = apply(prj(psi), gt.Fd(8), {3, 1, 2});
+    std::cout << disp(y) << "\n\n";
+    std::cout << norm(x - y) << "\n";
 }
