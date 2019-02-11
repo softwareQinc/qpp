c72173a74acd3e50527e806c9e5052976781aec1
Vlad Gheorghiu <vsoftco@gmail.com>
2019-02-11 10:37:10 -0500

    commit

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -6,4 +6,40 @@
 int main() {
     /////////// testing ///////////
     using namespace qpp;
+
+    QCircuit qc(16, 0);
+    idx i;
+    idx N = 1000;
+
+    std::cout << "1 qubit gates...\n";
+    try {
+        for (i = 0; i < N; ++i)
+            qc.gate(rand<cmat>(2, 2), 0);
+    } catch (...) {
+        std::cerr << "Collision at step: " << i << '\n';
+        throw;
+    }
+    std::cout << "2 qubit gates...\n";
+    try {
+        for (i = 0; i < N; ++i)
+            qc.gate(randU(4), 0, 1);
+    } catch (...) {
+        std::cerr << "Collision at step: " << i << '\n';
+        throw;
+    }
+    std::cout << "3 qubit gates...\n";
+    try {
+        for (i = 0; i < N; ++i)
+            qc.gate(randU(8), 0, 1, 2);
+    } catch (...) {
+        std::cerr << "Collision at step: " << i << '\n';
+        throw;
+    }
+
+    std::cout << "Executing...\n";
+    QEngine qEngine{qc};
+    for (auto&& elem : qc) {
+        std::cout << elem << '\n';
+        qEngine.execute(elem);
+    }
 }
