cf470e344d53509867bd996b3eadbe8c747cf2b7
Vlad Gheorghiu <vsoftco@gmail.com>
2019-02-16 14:12:56 -0500

    commit

---
+++ VisualStudio/qpp/qpp.cpp
@@ -0,0 +1,14 @@
+﻿// qpp.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+
+#include <iostream>
+#include "qpp.h"
+
+int main()
+{
+	using namespace qpp;
+	std::cout << "Hello, Quantum++!\nThis is the |0> state:\n";
+	std::cout << disp(st.z0) << '\n';
+	std::cin.get(); 
+}
