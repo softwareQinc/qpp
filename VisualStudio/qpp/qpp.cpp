aa434d7cfc8a28614f32d2853f077e69c6c22e14
Vlad Gheorghiu <vsoftco@gmail.com>
2019-02-16 11:21:06 -0500

    Update qpp.cpp

--- VisualStudio/qpp/qpp.cpp
+++ VisualStudio/qpp/qpp.cpp
@@ -5,11 +5,10 @@
 #include <iostream>
 #include "qpp.h"
 
-using namespace qpp;
-
 int main()
 {
+	using namespace qpp;
 	std::cout << "Hello, Quantum++!\nThis is the |0> state:\n";
 	std::cout << disp(st.z0) << '\n';
-	std::cin.get();
+	std::cin.get(); 
 }
