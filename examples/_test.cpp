f156f60a26675e6c955e329c522526d0de994cfa
Vlad Gheorghiu <vsoftco@gmail.com>
2019-02-23 16:13:21 -0500

    Update _test.cpp

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -6,4 +6,12 @@
 int main() {
     /////////// testing ///////////
     using namespace qpp;
+
+    // Quantum coin flip
+    QCircuit qc{1, 1};
+    qc.gate(gt.H, 0).measureZ(0, 0);
+
+    QEngine q_engine{qc};
+    q_engine.execute();
+    std::cout << q_engine << '\n';
 }
