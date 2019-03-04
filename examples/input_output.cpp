8b57de6ddc78d35ca60ca815b60d6970f11124a2
Vlad Gheorghiu <vsoftco@gmail.com>
2019-03-04 16:41:11 -0500

    commit

--- examples/input_output.cpp
+++ examples/input_output.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 
 #include "qpp.h"
-#include "MATLAB/matlab.h" // must be explicitly included
 
 int main() {
     using namespace qpp;
@@ -13,12 +12,5 @@ int main() {
     cmat loaded_rho = load<cmat>("rho.dat"); // load it back
     // display the difference in norm, should be 0
     std::cout << ">> Norm difference load/save: ";
-    std::cout << norm(loaded_rho - rho) << '\n';
-
-    // interfacing with MATLAB
-    saveMATLAB(rho, "rho.mat", "rho", "w");
-    loaded_rho = loadMATLAB<cmat>("rho.mat", "rho");
-    // display the difference in norm, should be 0
-    std::cout << ">> Norm difference MATLAB load/save: ";
     std::cout << norm(loaded_rho - rho) << '\n';
 }
---
+++ examples/matlab.cpp
@@ -0,0 +1,17 @@
+// MATLAB input/output
+// Source: ./examples/matlab.cpp
+#include <iostream>
+
+#include "qpp.h"
+#include "MATLAB/matlab.h" // must be explicitly included
+
+int main() {
+    using namespace qpp;
+    // interfacing with MATLAB
+    cmat rho = randrho(256); // an 8 qubit density operator
+    saveMATLAB(rho, "rho.mat", "rho", "w");
+    cmat loaded_rho = loadMATLAB<cmat>("rho.mat", "rho");
+    // display the difference in norm, should be 0
+    std::cout << ">> Norm difference MATLAB load/save: ";
+    std::cout << norm(loaded_rho - rho) << '\n';
+}
