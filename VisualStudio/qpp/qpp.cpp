cc1267f4c354ba48b50efa4cf2cc7b7121a71aca
Vlad Gheorghiu <vsoftco@gmail.com>
2019-03-25 14:26:44 -0400

    commit

--- VisualStudio/qpp/qpp.cpp
+++ VisualStudio/qpp/qpp.cpp
@@ -1,8 +1,6 @@
-﻿// qpp.cpp : Defines the entry point for the console application.
-//
-#include "stdafx.h"
-
+﻿// Minimal example, Windows platform
 #include <iostream>
+
 #include "qpp.h"
 
 int main()
