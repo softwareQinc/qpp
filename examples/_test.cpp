a0d53c4b07513b5cfec85a32f5d308d5b0286405
Vlad Gheorghiu <vsoftco@gmail.com>
2019-06-11 09:37:01 -0400

    commit

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -6,12 +6,4 @@
 int main() {
     /////////// testing ///////////
     using namespace qpp;
-
-    // Quantum coin flip
-    QCircuit qc{1, 1};
-    qc.gate(gt.H, 0).measureZ(0, 0);
-
-    QEngine q_engine{qc};
-    q_engine.execute();
-    std::cout << q_engine.get_dit(0) << '\n';
 }
