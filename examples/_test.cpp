3a1f1a61fe1e2087e27d2687bf2d35c00d949756
Vlad Gheorghiu <vsoftco@gmail.com>
2019-08-08 14:30:33 -0400

    commit

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -7,8 +7,12 @@ int main() {
     /////////// testing ///////////
     using namespace qpp;
 
-    QCircuit qc{4, 3, 3};
-    qc.add_circuit(adjoint(QCircuit{4, 2, 3}.QFT()), 0);
+    QCircuit qc{1, 2};
+    qc.cCTRL(gt.X, 0, 0, 1);
+    qc.measureZ(0, 1);
     std::cout << qc << "\n\n";
-    std::cout << qc.to_JSON() << "\n\n";
+
+    QEngine q_engine{qc};
+    q_engine.execute();
+    std::cout << q_engine << "\n\n";
 }
