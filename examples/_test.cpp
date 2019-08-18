01f91d6157127d451e465695dac06ebfe8c272fe
Vlad Gheorghiu <vsoftco@gmail.com>
2019-08-18 12:44:22 -0400

    commit

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -11,6 +11,7 @@ int main() {
     qc.gate_fan(gt.Fd(3));
     qc.measureZ({0, 1, 2}, 0, false);
     qc.measureZ({0, 1, 2}, 1, false);
+    qc.measureZ({0, 1, 2}, 2);
     std::cout << qc << "\n\n";
     std::cout << qc.to_JSON() << "\n\n";
 
