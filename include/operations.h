979f8a7fa31206228c49d827757f6b572f58fb7e
Vlad Gheorghiu <vsoftco@gmail.com>
2019-09-11 12:13:38 +0200

    Update operations.h

--- include/operations.h
+++ include/operations.h
@@ -125,7 +125,7 @@ applyCTRL(const Eigen::MatrixBase<Derived1>& state,
     // END EXCEPTION CHECKS
 
     if (shift.empty())
-        shift = std::vector<idx>(ctrl.size(), 0);
+        shift = {0};
 
     // construct the table of A^i and (A^dagger)^i
     std::vector<dyn_mat<typename Derived1::Scalar>> Ai;
@@ -208,8 +208,12 @@ applyCTRL(const Eigen::MatrixBase<Derived1>& state,
             for (idx k = 0; k < targetsize; ++k) {
                 Cmidx[target[k]] = CmidxA[k];
             }
-            coeff += Ai[(i_ + shift[i_]) % d](m_, n_) *
-                     rstate(internal::multiidx2n(Cmidx, n, Cdims));
+            if (ctrlsize > 0)
+                coeff += Ai[(i_ + shift[0]) % d](m_, n_) *
+                         rstate(internal::multiidx2n(Cmidx, n, Cdims));
+            else
+                coeff += Ai[i_](m_, n_) *
+                         rstate(internal::multiidx2n(Cmidx, n, Cdims));
         }
 
         return std::make_pair(coeff, indx);
