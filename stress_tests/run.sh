ecfac50940a3d60fe951d72a82b4732bd35e446b
Vlad Gheorghiu <vsoftco@gmail.com>
2020-11-26 09:04:16 -0500

    refactoring

--- stress_tests/run.sh
+++ stress_tests/run.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-NUM_CORES=8                      # number of cores
-NQ_START=2                       # number of qubits to start with
-NQ_END=24                        # number of qubits to end with
-DATE=`date '+%Y-%m-%d_%H-%M-%S'` # current date
-OUT_FILE=results_$DATE.csv       # results output file
+NUM_CORES=8                       # number of cores
+NQ_START=2                        # number of qubits to start with
+NQ_END=24                         # number of qubits to end with
+DATE=$(date '+%Y-%m-%d_%H-%M-%S') # current date
+OUT_FILE=results_$DATE.csv        # results output file
 
 echo "Stress test, see results_$DATE.csv"
 echo "num_cores num_qubits time_seconds"
-echo "numcores, nqubits, time_seconds" > $OUT_FILE
-for ((numcores=1; numcores <= NUM_CORES; numcores++));
-do
-    for ((n=NQ_START; n <= NQ_END; n++));
-    do
-        ./build/stress_test $numcores $n | tee -a $OUT_FILE 
-    done;
-done;
+echo "num_cores, n_qubits, time_seconds" >"$OUT_FILE"
+for ((num_cores = 1; num_cores <= NUM_CORES; num_cores++)); do
+    for ((n = NQ_START; n <= NQ_END; n++)); do
+        ./build/stress_test $num_cores $n | tee -a "$OUT_FILE"
+    done
+done
