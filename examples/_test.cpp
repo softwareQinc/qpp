a60bb86db5f8e022968aab7a631d884e13bf489d
Vlad Gheorghiu <vsoftco@gmail.com>
2020-12-06 12:52:16 -0500

    noisy engine timing test

--- examples/_test.cpp
+++ examples/_test.cpp
@@ -8,21 +8,20 @@ int main(int argc, char** argv) {
     /////////// testing ///////////
     using namespace qpp;
 
-    assert(argc > 1);
+    assert(argc > 2);
     std::size_t reps = std::stoi(argv[1]);
+    bool noisy = std::stoi(argv[2]);
 
-    std::size_t nq = 2, nc = nq;
+    std::size_t nq = 5, nc = nq;
     QCircuit qCircuit{nq, nc};
 
     qCircuit.gate(gt.H, 0);
     qCircuit.CTRL(gt.X, 0, 1);
     for (std::size_t i = 0; i < nc; ++i)
         qCircuit.measureZ(i, i);
-    std::cout << qCircuit << '\n';
+    std::cout << qCircuit << "\n\n";
 
     std::unique_ptr<QEngine> qEngine;
-    bool noisy = true;
-
     if (noisy)
         qEngine.reset(new QNoisyEngine<QubitBitFlipNoise>{
             qCircuit, QubitBitFlipNoise{0.01}});
