296b7b84ff0acb74b437ff97f091383d3a0ed35a
Vlad Gheorghiu <vsoftco@gmail.com>
2021-07-14 08:05:01 -0400

    BB84 example

--- examples/BB84.cpp
+++ examples/BB84.cpp
@@ -1,6 +1,8 @@
 // BB84
 // Source: ./examples/BB84.cpp
+#include <cstdlib>
 #include <iostream>
+#include <numeric>
 #include <tuple>
 #include <utility>
 #include <vector>
@@ -14,21 +16,22 @@ using key_T = std::vector<short>; // key type (bit string)
 // display Alice's and Bob's bases choices and states
 void display(const bases_states_T& Alice_bases_states,
              const bases_states_T& Bob_bases_states) {
+    using namespace qpp;
     auto n = Alice_bases_states.size();
     std::cout << "Alice's states: ";
-    for (std::size_t i = 0; i < n; ++i)
+    for (idx i = 0; i < n; ++i)
         std::cout << Alice_bases_states[i].second << ' ';
     std::cout << '\n';
     std::cout << "Alice's bases:  ";
-    for (std::size_t i = 0; i < n; ++i)
+    for (idx i = 0; i < n; ++i)
         std::cout << (Alice_bases_states[i].first == 0 ? 'Z' : 'X') << ' ';
     std::cout << '\n';
     std::cout << "Bob's bases:    ";
-    for (std::size_t i = 0; i < n; ++i)
+    for (idx i = 0; i < n; ++i)
         std::cout << (Bob_bases_states[i].first == 0 ? 'Z' : 'X') << ' ';
     std::cout << '\n';
     std::cout << "Bob's states:   ";
-    for (std::size_t i = 0; i < n; ++i)
+    for (idx i = 0; i < n; ++i)
         std::cout << Bob_bases_states[i].second << ' ';
     std::cout << '\n';
 }
@@ -36,9 +39,10 @@ void display(const bases_states_T& Alice_bases_states,
 // key sifting, removes the locations where the bases do not coincide
 void key_sift(bases_states_T& Alice_bases_states,
               bases_states_T& Bob_bases_states) {
+    using namespace qpp;
     auto n = Alice_bases_states.size();
     bases_states_T result_A, result_B;
-    for (std::size_t i = 0; i < n; ++i) {
+    for (idx i = 0; i < n; ++i) {
         if (Alice_bases_states[i].first != Bob_bases_states[i].first)
             continue;
         result_A.emplace_back(Alice_bases_states[i]);
@@ -48,21 +52,76 @@ void key_sift(bases_states_T& Alice_bases_states,
     Bob_bases_states = result_B;
 }
 
-// retrieves the raw key
-key_T raw_key(const bases_states_T& bases_states) {
-    auto n = bases_states.size();
-    key_T result(n);
-    for (std::size_t i = 0; i < n; ++i)
-        result[i] = bases_states[i].second;
-    return result;
+// Alice and Bob check a subset (of size k) of their qubits and estimate how
+// much Eve eavesdropped; returns the number of positions where they disagree
+double check_eavesdrop(bases_states_T& Alice_bases_states,
+                       bases_states_T& Bob_bases_states, qpp::idx k) {
+    using namespace qpp;
+    auto n = Alice_bases_states.size();
+
+    if (k > n) {
+        std::cout << ">> Not enough check qubits (k too large), aborting...\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<idx> pos(n);
+    std::iota(pos.begin(), pos.end(), 0);
+    auto& gen =
+#ifdef NO_THREAD_LOCAL_
+        RandomDevices::get_instance().get_prng();
+#else
+        RandomDevices::get_thread_local_instance().get_prng();
+#endif
+    // first k elements label the qubits we want to check
+    std::shuffle(pos.begin(), pos.end(), gen);
+    // sort (first k of them) for std::binary_search() later
+    std::sort(pos.begin(), std::next(pos.begin(), k));
+
+    bases_states_T result_A, result_B;
+    idx cnt = 0; // how many bits differ
+    for (idx i = 0; i < n; ++i) {
+        // is current position part of the ones Alice and Bob need to check?
+        if (std::binary_search(pos.begin(), std::next(pos.begin(), k), i)) {
+            auto basis_AB = Alice_bases_states[i].first;
+            auto state_A = Alice_bases_states[i].second;
+            auto state_B = Bob_bases_states[i].second;
+
+            ket psi_A = (state_A == 0) ? 0_ket : 1_ket;
+            ket psi_B = (state_B == 0) ? 0_ket : 1_ket;
+            cmat U = gt.Z;     // measurement basis
+            if (basis_AB != 0) // if X basis
+            {
+                U = gt.H;
+                psi_A = gt.H * psi_A;
+                psi_B = gt.H * psi_B;
+            }
+
+            auto measure_A = measure(psi_A, U);
+            auto m_A = std::get<RES>(measure_A); // Alice's measurement result
+
+            auto measure_B = measure(psi_B, U);
+            auto m_B = std::get<RES>(measure_B); // Bob's measurement result
+
+            if (m_A != m_B)
+                ++cnt;
+        } else {
+            result_A.emplace_back(Alice_bases_states[i]);
+            result_B.emplace_back(Bob_bases_states[i]);
+        }
+    }
+    Alice_bases_states = result_A;
+    Bob_bases_states = result_B;
+
+    return static_cast<double>(cnt) / static_cast<double>(k);
 }
 
-// computes the final key; technically, here is where we do error correction and
+// compute the final key; technically, here is where we do error correction and
 // privacy amplification; however, here we simply discard the bits that differ
 key_T final_key(const key_T& Alice_raw_key, const key_T& Bob_raw_key) {
+    using namespace qpp;
     auto n = Alice_raw_key.size();
     key_T result;
-    for (std::size_t i = 0; i < n; ++i) {
+    for (idx i = 0; i < n; ++i) {
         if (Alice_raw_key[i] != Bob_raw_key[i])
             continue;
         result.emplace_back(Alice_raw_key[i]);
@@ -70,17 +129,34 @@ key_T final_key(const key_T& Alice_raw_key, const key_T& Bob_raw_key) {
     return result;
 }
 
+// helper, retrieves the key from a collection of (basis, state) pairs
+key_T raw_key(const bases_states_T& bases_states) {
+    using namespace qpp;
+    auto n = bases_states.size();
+    key_T result(n);
+    for (idx i = 0; i < n; ++i)
+        result[i] = bases_states[i].second;
+    return result;
+}
+
 int main() {
     using namespace qpp;
 
-    std::size_t n = 100; // number of qubits Alice sends to Bob
+    idx n = 100;    // no. of qubits Alice sends to Bob
+    idx k = 20;     // no. of qubits Alice and Bob check for eavesdropping
     double p = 0.5; // probability of Eve intercepting (and altering) the qubits
+
+    // when we should abort due to eavesdropping; lower in reality
+    double abort_rate = 0.2;
+
     std::cout << ">> BB84, sending n = " << n << " qubits from Alice to Bob\n";
     std::cout << ">> With probability p = " << p
               << " Eve intercepts the qubits and randomly measures them in the "
                  "Z or X basis, then sends them to Bob\n";
+    std::cout << ">> Excludes error correction and privacy amplification\n";
 
-    // Alice's basis and state pairs (basis, state); 0 -> Z basis, 1 -> X basis
+    // Alice's basis and state pairs (basis, state); 0 -> Z basis, 1 -> X
+    // basis
     bases_states_T Alice_bases_states(n);
     for (auto& elem : Alice_bases_states) {
         // chose a random basis, 0 -> Z basis, 1 -> X basis
@@ -90,7 +166,8 @@ int main() {
         elem = std::make_pair(basis, state);
     }
 
-    // Bob's basis and state pairs (basis, state); 0 -> Z basis, 1 -> X basis
+    // Bob's basis and state pairs (basis, state); 0 -> Z basis, 1 -> X
+    // basis
     bases_states_T Bob_bases_states(n);
     for (auto& elem : Bob_bases_states) {
         // chose a random basis, 0 -> Z basis, 1 -> X basis
@@ -99,9 +176,9 @@ int main() {
     }
 
     // Alice "prepares" the qubits and "sends" them to Bob one by one
-    // Eve is in the middle and intercepts/resends the qubits with probability p
-    // Bob measures the received qubits one by one
-    for (std::size_t i = 0; i < n; ++i) {
+    // Eve is in the middle and intercepts/resends the qubits with
+    // probability p Bob measures the received qubits one by one
+    for (idx i = 0; i < n; ++i) {
         auto basis_A = Alice_bases_states[i].first;
         auto state_A = Alice_bases_states[i].second;
         ket psi = (state_A == 0) ? 0_ket : 1_ket;
@@ -115,8 +192,8 @@ int main() {
             short basis_E = bernoulli();
             cmat U_E = (basis_E == 0) ? gt.Z : gt.H;
             auto measure_E = measure(psi, U_E);
-            auto m = std::get<RES>(measure_E); // measurement result
-            psi = std::get<ST>(measure_E)[m];  // update the state accordingly
+            auto m_E = std::get<RES>(measure_E); // measurement result
+            psi = std::get<ST>(measure_E)[m_E];  // update the state accordingly
         }
 
         // Bob measures the qubit Eve re-sent
@@ -124,7 +201,9 @@ int main() {
         // Bob's measurement eigenvectors
         cmat U_B = (basis_B == 0) ? gt.Z : gt.H;
         auto measure_B = measure(psi, U_B);
-        Bob_bases_states[i].second = std::get<RES>(measure_B);
+        auto m_B = std::get<RES>(measure_B); // measurement result
+        Bob_bases_states[i].second =
+            static_cast<decltype(Bob_bases_states[i].second)>(m_B);
     }
 
     // display the results before bases sifting
@@ -134,8 +213,18 @@ int main() {
     // filter on same bases
     key_sift(Alice_bases_states, Bob_bases_states);
 
-    // display the results after basis sifting
-    std::cout << ">> After sifting (raw key)\n";
+    // check eavesdropping
+    auto eves_rate = check_eavesdrop(Alice_bases_states, Bob_bases_states, k);
+    std::cout << ">> Detected eavesdropping rate: " << eves_rate << '\n';
+    // if rate is too high we should abort here
+    if (eves_rate > abort_rate) {
+        std::cout
+            << ">> Detected eavesdropping rate is too high, aborting...\n";
+        return EXIT_FAILURE;
+    }
+
+    // display the results after basis sifting and eavesdropping detection
+    std::cout << ">> After sifting and eavesdrop detection (raw key)\n";
     display(Alice_bases_states, Bob_bases_states);
 
     std::cout << ">> Established keys\n";
