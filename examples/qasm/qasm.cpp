5d31031e7d3e5271bac8d383d0665c1ea545164c
Vlad Gheorghiu <vsoftco@gmail.com>
2021-10-06 12:58:06 -0400

    Update qasm.cpp

--- examples/qasm/qasm.cpp
+++ examples/qasm/qasm.cpp
@@ -8,13 +8,8 @@
 int main(int argc, char** argv) {
     using namespace qpp;
 
-    QCircuit qc;
     // read the circuit from the input stream
-    qc = qasm::read(std::cin);
-
-    idx reps = 1;
-    if (argc > 1)
-        reps = std::stoi(argv[1]);
+    QCircuit qc = qasm::read(std::cin);
 
     // initialize the quantum engine with a circuit
     QEngine q_engine{qc};
@@ -23,15 +18,15 @@ int main(int argc, char** argv) {
     std::cout << qc << "\n\n" << qc.get_resources() << "\n\n";
 
     // execute the quantum circuit
+    idx reps = argc > 1 ? std::stoi(argv[1]) : 1; // repetitions
     q_engine.execute(reps);
 
     // display the measurement statistics
     std::cout << q_engine << '\n';
 
-    // uncomment the following lines if you want to display the final state
-    /*
-    ket psi_final = q_engine.get_psi();
-    std::cout << ">> Final state (transpose):\n"
-              << disp(transpose(psi_final)) << '\n';
-    */
+    // display the final state on demand
+    if (argc > 2) {
+        std::cout << ">> Final state (transpose):\n";
+        std::cout << disp(transpose(q_engine.get_psi())) << '\n';
+    }
 }
