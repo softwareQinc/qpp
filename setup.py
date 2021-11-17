ef2b7edc2addf2aa0cc8210c99ecd7e8b13cec8a
Scott Xu <scottx@untether.ai>
2021-11-17 09:21:04 -0500

    Remove static Eigen dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from glob import glob
 from setuptools import setup
 from libs.pybind11.setup_helpers import Pybind11Extension
+import os
+import subprocess
+
+p = subprocess.Popen("cmake pyqpp",
+                     shell=True,
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.STDOUT)
+
+prefix = "Detecting Eigen3 - done in "
+eigen_path = None
+for line in p.stdout.read().decode('ascii').split('\n'):
+    if line.startswith(prefix):
+        eigen_path = line[len(prefix):]
+        break
+
+if eigen_path is None:
+    raise Exception('Eigen3 not found!')
 
 ext_modules = [
     Pybind11Extension(
         "pyqpp",
         ["pyqpp/qpp_wrapper.cpp"],
-        extra_compile_args=["-Ilibs", "-Iinclude", "-Iqasmtools/include"],
+        extra_compile_args=["-Ilibs", "-Iinclude", "-Iqasmtools/include", "-I"+eigen_path],
         cxx_std=17,
         include_pybind11=False,
     ),
