4a6641dc22c374b4e42a4cdc04e3f99a2b084b99
Vlad Gheorghiu <vsoftco@gmail.com>
2023-11-30 12:10:47 -0500

    Update qpp_dependencies.cmake

--- cmake/qpp_dependencies.cmake
+++ cmake/qpp_dependencies.cmake
@@ -266,7 +266,6 @@ if (MSVC)
     # Disable spurious Eigen warnings with MSVC (warning STL4007)
     add_compile_definitions(_SILENCE_CXX17_ADAPTOR_TYPEDEFS_DEPRECATION_WARNING)
     add_compile_options(-bigobj)
-    add_compile_definitions(NOMINMAX)
 endif ()
 
 #### MinGW or Cygwin have issues with object files that are too large
