df5cec2463a4b0df639e3e0bc2259b1d55e8226b
Vlad Gheorghiu <vsoftco@gmail.com>
2024-02-13 15:42:06 -0500

    c

--- cmake/qpp_eigen3.cmake
+++ cmake/qpp_eigen3.cmake
@@ -1,6 +1,6 @@
 message(STATUS "Detecting Eigen3...")
 find_package(Eigen3 NO_MODULE QUIET)
-if(NOT EIGEN3_INCLUDE_DIR)
+if(NOT TARGET Eigen3::Eigen)
   # Install Eigen3 on demand
   include(FetchContent)
   set(FETCHCONTENT_QUIET FALSE)
