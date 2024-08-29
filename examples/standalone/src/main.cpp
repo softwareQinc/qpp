c4cb662cc346f8af59c944ca101fce1416ccc59b
Vlad Gheorghiu <vsoftco@gmail.com>
2024-08-29 16:43:08 -0400

    Windows fix
    Signed-off-by: Vlad Gheorghiu <vsoftco@gmail.com>

--- examples/standalone/src/main.cpp
+++ examples/standalone/src/main.cpp
@@ -11,7 +11,8 @@ int main() {
     QCircuit qc{1, 1, 2, "coin flip"};
     qc.gate(gt.H, 0);
     qc.measure_all();
-
     std::cout << qc << "\n\n" << qc.get_resources() << "\n\n";
-    std::cout << QEngine{qc}.execute(100) << "\n";
+
+    QEngine qe{qc};
+    std::cout << qe.execute(100) << "\n";
 }
