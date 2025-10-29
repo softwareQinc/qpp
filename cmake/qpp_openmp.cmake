94966951d9f47e3f1bd57de88cf7be5e2e17b648
Vlad Gheorghiu <vsoftco@gmail.com>
2025-10-29 19:02:15 -0400

    Fix macOS
    Signed-off-by: Vlad Gheorghiu <vsoftco@gmail.com>

--- cmake/qpp_openmp.cmake
+++ cmake/qpp_openmp.cmake
@@ -2,7 +2,7 @@
 option(QPP_OPENMP "Enable OpenMP support" ON)
 
 if(QPP_OPENMP)
-  find_package(OpenMP REQUIRED)
+  find_package(OpenMP 3.0 COMPONENTS CXX)
   if(OpenMP_CXX_FOUND)
     target_compile_definitions(libqpp INTERFACE QPP_OPENMP)
     target_link_libraries(libqpp INTERFACE OpenMP::OpenMP_CXX)
